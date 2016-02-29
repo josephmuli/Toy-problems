30089c479cb85c9e0dcce240a3ad2618886ba225
joseph muli <jayjaymuli00@gmail.com>
2016-02-29 13:09:07 +0300

    capitalize-py

---
+++ python/capitalize/capiatlize.py
@@ -0,0 +1,22 @@
+# Using Python, have the function capitalize(str) take the str parameter being passed and capitalize
+# the first letter of each word. Words will be separated by only one space.
+# it should take string input from a user
+# do not use the capitalize method
+
+
+def capitalize():
+	str = input("Enter your Word")
+	
+
+	for word in str:
+		word.split()
+		#print(y +str[1:])
+		y = str[0].upper()
+		z = y +str[1:]
+		print(z)
+
+
+
+
+
+capitalize()
